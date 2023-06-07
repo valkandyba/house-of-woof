0c1619013a49ba7bcada7bc8bf76bf473418ff2e
Valentina Kandyba <valentina.kandyba@dataart.com>
2023-06-07 18:44:21 +0200

    initial structure

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { HomePageProps } from "./types";
+
+import Header from "../../components/Header/Header";
+
+const HomePage: React.FC<HomePageProps> = ({}) => {
+  return <Header />;
+};
+
+export default HomePage;
