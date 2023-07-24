545cc4fe71bdb5334ca3708f67d8a8c2875557ee
Valentina Kandyba <valentina.kandyba@dataart.com>
2023-07-24 17:33:42 +0200

    feature: ECA-11 wish list page (fixes according to comments)

--- src/components/WishList/WishList.tsx
+++ src/components/WishList/WishList.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Grid } from '@mui/material';
 import WishListItem from './WishListItem/WishListItem';
-import './WishList.module.scss';
 
 interface WishListProps {
   favoriteItems: FavoriteItem[];
