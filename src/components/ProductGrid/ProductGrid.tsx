55e73705e2049bd762d6873216cdc7ae8c669950
Valentina Kandyba <valentina.kandyba@dataart.com>
2023-08-07 14:58:24 +0200

    feature: ECA-21 add fetch request (fixes according to comment)

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Grid } from '@mui/material';
+import { Button, Container, Grid } from '@mui/material';
 import ProductItem from '../ProductItem/ProductItem';
 import classes from './ProductGrid.module.scss';
 import useAxios from '../../hooks/useAxios';
@@ -8,7 +8,7 @@ import { CartItem } from '../../store/types';
 const ProductGrid: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const { response, loading } = useAxios({
-    url: `https://64876d13beba62972790a0da.mockapi.io/api/v1/products?page=${currentPage}&limit=10`,
+    url: `https://64876d13beba62972790a0da.mockapi.io/api/v1/products?page=${currentPage}&limit=12`,
     method: 'get',
   });
 
@@ -22,14 +22,20 @@ const ProductGrid: React.FC = () => {
     <Container maxWidth='xl'>
       <div className={classes.products}>
         {loading && 'loading'}
-        <Grid container spacing={3}>
+        <Grid container spacing={3} mb={5}>
           {products.map((product: CartItem) => (
             <Grid item xs={12} sm={6} md={3} key={product.id}>
               <ProductItem {...product} />
             </Grid>
           ))}
         </Grid>
-        <button onClick={handlerLoadMore}>Load More</button>
+        {response && (
+          <div className={classes['btn-holder']}>
+            <Button size='large' variant='contained' onClick={handlerLoadMore}>
+              Load More
+            </Button>
+          </div>
+        )}
       </div>
     </Container>
   );
