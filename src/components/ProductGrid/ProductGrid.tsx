b9f60039ae9148f69f08fc67a714fae30d8f9036
Valentina Kandyba <valentina.kandyba@dataart.com>
2023-08-07 13:36:18 +0200

    feature: ECA-21 add fetch request (fixes according to comments)

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Grid } from '@mui/material';
 import ProductItem from '../ProductItem/ProductItem';
 import classes from './ProductGrid.module.scss';
@@ -6,10 +6,15 @@ import useAxios from '../../hooks/useAxios';
 import { CartItem } from '../../store/types';
 
 const ProductGrid: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const { response, loading } = useAxios({
-    url: 'https://64876d13beba62972790a0da.mockapi.io/api/v1/products?page=1&limit=10',
+    url: `https://64876d13beba62972790a0da.mockapi.io/api/v1/products?page=${currentPage}&limit=10`,
     method: 'get',
   });
+
+  const handlerLoadMore = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   const products: CartItem[] = Array.isArray(response) ? response : [];
 
@@ -24,6 +29,7 @@ const ProductGrid: React.FC = () => {
             </Grid>
           ))}
         </Grid>
+        <button onClick={handlerLoadMore}>Load More</button>
       </div>
     </Container>
   );
